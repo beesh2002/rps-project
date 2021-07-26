db8b2e7caeafaebf53eef4efad802083603a9538
Bashar <32504733+beesh2002@users.noreply.github.com>
2021-07-26 14:02:56 -0500

    initial test

--- rps/game.js
+++ rps/game.js
@@ -5,43 +5,30 @@ class Game{
         this.countOfComputerWins = 0;
         this.countOfTies = 0;
     }
-    incrementPlayerWins(){
-        //*********************************ADD CODE HERE *************************************/
-        // Add 1 to the countOfPlayerWins property
-    }
+    incrementPlayerWins(){                   this.countOfPlayerWins=+1;            }
 
-    incrementComputerWins(){
-        //*********************************ADD CODE HERE *************************************/
-        // Add 1 to the countOfComputerWins property
-    }
+    incrementComputerWins(){                 this.countOfComputerWins=+1;          }
 
-    incrementCountOfTies(){
-        //*********************************ADD CODE HERE *************************************/
-        // Add 1 to the countOfTies property
-    }
+    incrementCountOfTies(){                  this.countOfTies=+1;                  }
 
-
-    setNumberOfRounds(numberOfRounds){
-        //*********************************ADD CODE HERE *************************************/
-        // Set the numberOfRounds property to the parameter
-    }
+    setNumberOfRounds(numberOfRounds){       this.numberOfRounds=numberOfRounds;   }
 
     newGame(){
-        //*********************************ADD CODE HERE *************************************/
-        // Clear all properties to their default values
+        
+        this.numberOfRounds = null;
+        this.countOfPlayerWins = 0;
+        this.countOfComputerWins = 0;
+        this.countOfTies = 0;
     }
 
      /**
      * tests the numberOfRounds global variable and determines if it is a valid value
      * @returns boolean
      */
-     isNumberOfRoundsValid(num) {
-        //*********************************ADD CODE HERE *************************************/
-        // return a boolean value that shows if the parameter is a valid user input (A number from 1 - 5)
-    }
+     isNumberOfRoundsValid(num) {       return (num=1? true:(num=2? true:(num=3?true:(num=4?true:(num=5?true:false)))));   }
 }
 
 
 
-// DO NOT TOUCH THIS CODE
-let unit_test  = Game;
+// DO NOT TOUCH THIS CODE -- No I will not TOUCH IT
+let unit_test  = Game; //Just did
